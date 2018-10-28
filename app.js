a16e30478d96a9ac435354e34f249080dbc7ac4b
paulappz <pauloluyege@gmail.com>
2018-10-28 06:07:03 +0100

    up

--- app.js
+++ app.js
@@ -7,25 +7,25 @@ var app = new Vue({
         Customer_id: "",
         dataloaded: false,
         tabs: [{
-                name: "3 Days Reminder SMS",
-                id: 0,
-                isActive: true
-            },
-            {
-                name: "1 Day Overdue SMS",
-                id: 1,
-                isActive: false
-            },
-            {
-                name: "16 Days Overdue SMS/CALL",
-                id: 2,
-                isActive: false
-            },
-            {
-                name: "31 Days Overdue SMS/VISIT",
-                id: 3,
-                isActive: false
-            }
+            name: "3 Days Reminder SMS",
+            id: 0,
+            isActive: true
+        },
+        {
+            name: "1 Day Overdue SMS",
+            id: 1,
+            isActive: false
+        },
+        {
+            name: "16 Days Overdue SMS/CALL",
+            id: 2,
+            isActive: false
+        },
+        {
+            name: "31 Days Overdue SMS/VISIT",
+            id: 3,
+            isActive: false
+        }
 
         ],
         activeTab: {},
@@ -37,11 +37,15 @@ var app = new Vue({
         select_call: false,
         checKiD: [],
         phoneNo: '',
-        CustName: ''
+        CustName: '',
+        dep_startDate: '',
+        dep_endDate: '',
+        inv_startDate: '',
+        inv_endDate: ''
 
     },
 
-    mounted: function() {
+    mounted: function () {
         console.log('mounted');
 
         this.getReminderSMS();
@@ -53,19 +57,19 @@ var app = new Vue({
     computed: {
 
     },
-    ready: function() {
+    ready: function () {
         this.setActive(this.tabs[0]);
 
     },
     methods: {
 
-        setActive: function(tab) {
+        setActive: function (tab) {
             var self = this;
             tab.isActive = true;
             this.activeTab = tab;
             /*this.activeTab.isActive = true;*/
             console.log("activeTab name=" + this.activeTab.name);
-            this.tabs.forEach(function(tab) {
+            this.tabs.forEach(function (tab) {
                 console.log("TAB => " + tab);
                 console.log("activeTab id => " + self.activeTab.id);
                 console.log("tab id=" + tab.id);
@@ -77,9 +81,9 @@ var app = new Vue({
             });
         },
 
-        getReminderSMS: function() {
+        getReminderSMS: function () {
             axios.get("https://altara-api.herokuapp.com/api.php?action=autoreminder")
-                .then(function(response) {
+                .then(function (response) {
                     console.log(response);
                     if (response.data.error) {
                         app.errorMessage = response.data.message;
@@ -88,9 +92,9 @@ var app = new Vue({
                     }
                 });
         },
-        get1DayOverdue: function() {
+        get1DayOverdue: function () {
             axios.get("https://altara-api.herokuapp.com/api.php?action=overdue1")
-                .then(function(response) {
+                .then(function (response) {
                     /*   console.log(response); */
                     if (response.data.error) {
                         app.errorMessage = response.data.message;
@@ -99,9 +103,9 @@ var app = new Vue({
                     }
                 });
         },
-        get16DaysOverdue: function() {
+        get16DaysOverdue: function () {
             axios.get("https://altara-api.herokuapp.com/api.php?action=overdue2")
-                .then(function(response) {
+                .then(function (response) {
                     /*   console.log(response); */
                     if (response.data.error) {
                         app.errorMessage = response.data.message;
@@ -111,9 +115,9 @@ var app = new Vue({
                 });
         },
 
-        get31DaysOverdue: function() {
+        get31DaysOverdue: function () {
             axios.get("https://altara-api.herokuapp.com/api.php?action=overdue3")
-                .then(function(response) {
+                .then(function (response) {
                     app.dataloaded = false;
                     /*   console.log(response); */
                     if (response.data.error) {
@@ -126,12 +130,55 @@ var app = new Vue({
         },
 
 
-        downloadCustomerData: function() {
+        downloadCustomerData: function () {
             app.dataloaded = true;
             console.log(app.Regdate);
 
             axios.post("https://altara-api.herokuapp.com/api.php?action=download", {
-                    RegDate: app.Regdate
+                RegDate: app.Regdate
+
+            }, { responseType: 'blob' }, {
+                    headers: {
+                        'Accept': 'Content-Type: text/csv',
+                    }
+                })
+                .then(function (response) {
+                    download(response.data, app.Regdate + 'customerdata.csv');
+                    app.Regdate = "";
+                    app.dataloaded = false;
+                });
+        },
+
+        Checkdate: function (startDate, endDate) {
+            var date1 = new Date(endDate);
+            var date2 = new Date(startDate);
+
+            if (startDate == '' || endDate == '') {
+                app.errorMessageChk = "Field can't be empty";
+                setTimeout(function () {
+                    app.errorMessageChk = '';
+                }, 1000);
+                return false
+            }
+            else if (((date1.setHours(0, 0, 0, 0)) <= (date2.setHours(0, 0, 0, 0))) || (Number(app.diffDays) > 7)) {
+                app.errorMessageChk = "InValid Date Range, Hint: StartDate must be greater than Enddate and Date difference must not exceed 6";
+                setTimeout(function () {
+                    app.errorMessageChk = '';
+                }, 1000);
+                return false
+            }
+            else return true
+
+
+        },
+
+        downloadDeposite: function (startDate, endDate) {
+            console.log(startDate + '  ' + endDate )
+            if (app.Checkdate(startDate, endDate)) {
+
+                axios.post("https://altara-api.herokuapp.com/api.php?action=download_dep", {
+                    startDate: startDate,
+                    endDate: endDate
 
                 }, { responseType: 'blob' }, {
                     headers: {
@@ -139,28 +186,71 @@ var app = new Vue({
                     }
                 })
                 .then(function(response) {
-                    download(response.data, app.Regdate + 'customerdata.csv');
-                    app.Regdate = "";
+                    if (response.data){
+                        download(response.data,  'Deposite from '+ startDate + 'to '+ endDate + '.csv');
+                    }
+                    else{
+                        app.errorMessageChk = "Error - Contact Support";
+                setTimeout(function () {
+                    app.errorMessageChk = '';
+                }, 1000);
+                    }
                     app.dataloaded = false;
                 });
+            }
+            else {
+
+            }
         },
 
-        ApproveCustomer: function(name, telnumber) {
+        downloadInvoice: function (startDate, endDate) {
+            console.log(startDate + '  ' + endDate )
+            if (app.Checkdate(startDate, endDate)) {
+
+                axios.post("https://altara-api.herokuapp.com/api.php?action=download_inv", {
+                    startDate: startDate,
+                    endDate: endDate
+
+                }, { responseType: 'blob' }, {
+                    headers: {
+                        'Accept': 'Content-Type: text/csv',
+                    }
+                })
+                .then(function(response) {
+                    if (response.data){
+                    download(response.data,  'Sales Invoice from '+ startDate + 'to '+ endDate + '.csv');
+                    }
+                    else{
+                        app.errorMessageChk = "Error - Contact Support";
+                setTimeout(function () {
+                    app.errorMessageChk = '';
+                }, 1000);
+                    }
+                    app.dataloaded = false;
+                });
+            }
+            else {
+
+            }
+        },
+
+
+        ApproveCustomer: function (name, telnumber) {
 
             console.log(app.Customer_id)
             if (app.checKiD.length == 1) {
 
                 axios.post("https://altara-api.herokuapp.com/api.php?action=approve", {
-                        Customer_id: app.Customer_id
-                    })
-                    .then(function(response) {
+                    Customer_id: app.Customer_id
+                })
+                    .then(function (response) {
                         console.log(response);
 
                         if (response.data.error) {
                             app.submitted = false;
                             app.errorMessage = response.data.message;
 
-                            setTimeout(function() {
+                            setTimeout(function () {
                                 app.errorMessage = '';
                             }, 1000);
 
@@ -168,7 +258,7 @@ var app = new Vue({
                             app.submitted = false;
                             app.successMessage = response.data.message;
                             app.sendNotification(name, telnumber)
-                            setTimeout(function() {
+                            setTimeout(function () {
                                 app.successMessage = '';
                             }, 1000);
 
@@ -177,7 +267,7 @@ var app = new Vue({
             } else {
                 app.errorMessage = "Customer ID Doesnt Exist";
 
-                setTimeout(function() {
+                setTimeout(function () {
                     app.errorMessage = '';
 
                 }, 1000);
@@ -187,19 +277,19 @@ var app = new Vue({
 
         },
 
-        CheckId: function() {
+        CheckId: function () {
 
             app.dataloaded = true;
             axios.post("https://altara-api.herokuapp.com/api.php?action=checkId", {
-                    Customer_id: app.Customer_id
-                })
-                .then(function(response) {
+                Customer_id: app.Customer_id
+            })
+                .then(function (response) {
                     console.log(response);
                     app.dataloaded = false;
                     if (response.data.error) {
                         app.errorMessage = response.data.message;
 
-                        setTimeout(function() {
+                        setTimeout(function () {
                             app.errorMessage = '';
                         }, 1000);
 
@@ -216,16 +306,16 @@ var app = new Vue({
         },
 
 
-        resetMessage: function() {
+        resetMessage: function () {
             app.errorMessage = "";
             app.successMessage = "";
         },
 
-        updateComment: function() {
+        updateComment: function () {
 
             if (app.comment.Res == undefined || app.comment.Comment == undefined) {
                 app.errorMessage = "Fill in all fields before submission";
-                setTimeout(function() {
+                setTimeout(function () {
                     app.errorMessage = '';
                 }, 2000);
             } else {
@@ -233,7 +323,7 @@ var app = new Vue({
                 var formData = app.toFormData(app.comment);
 
                 axios.post("https://altara-api.herokuapp.com/api.php?action=comment", formData)
-                    .then(function(response) {
+                    .then(function (response) {
                         console.log(response);
                         app.comment = {};
                         if (response.data.error) {
@@ -245,7 +335,7 @@ var app = new Vue({
                             app.get16DaysOverdue();
                             app.get31DaysOverdue();
                             app.successMessage = response.data.message;
-                            setTimeout(function() {
+                            setTimeout(function () {
                                 app.successMessage = '';
 
                             }, 2000);
@@ -260,7 +350,7 @@ var app = new Vue({
             console.log(app.comment.Transaction_id);
         },
 
-        toFormData: function(obj) {
+        toFormData: function (obj) {
             var form_data = new FormData();
             for (var key in obj) {
                 form_data.append(key, obj[key]);
@@ -275,19 +365,19 @@ var app = new Vue({
             // axios.get("http://api.smartsmssolutions.com/smsapi.php?username=bjmarcson&password=fabregas10&sender=Altara&recipient=" + telnumber + "&message=" + message + "")
             axios.get("https://api.infobip.com/sms/1/text/query?username=Oluwatoke12&password=Altara99&to=" + 234 + telnumber + "&text=" + message + "")
 
-            .then(function(response2) {
-                console.log(response2);
-                if (response2.status == 200) {
-                    app.successMessage = "Notification sent to Customer";
+                .then(function (response2) {
+                    console.log(response2);
+                    if (response2.status == 200) {
+                        app.successMessage = "Notification sent to Customer";
 
-                    setTimeout(function() {
-                        app.successMessage = '';
-                    }, 2000);
-                    // updateRemark(Updata)
-                } else {
-                    app.errorMessage = "Error Sending Message, Contact Support";
-                }
-            });
+                        setTimeout(function () {
+                            app.successMessage = '';
+                        }, 2000);
+                        // updateRemark(Updata)
+                    } else {
+                        app.errorMessage = "Error Sending Message, Contact Support";
+                    }
+                });
         }
 
     }
